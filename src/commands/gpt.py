7a0b90dc16349204023f9827d01b669729559d6d
Collin Dang <collindang88@gmail.com>
2023-04-19 06:01:28 +0000

    split gpt messages into chunks if too long

--- src/commands/gpt.py
+++ src/commands/gpt.py
@@ -15,6 +15,11 @@ async def gpt(ctx, *input):
             model="gpt-3.5-turbo",
             messages=messages
         )
-        await ctx.send(response.choices[0].message.content.strip())
+        response_content = response.choices[0].message.content.strip()
+        message_chunks = [response_content[i:i + 2000] for i in range(0, len(response_content), 2000)]
+
+        for chunk in message_chunks:
+            await ctx.send(chunk)
+            
     except Exception as e:
         await ctx.send(f"Error: {str(e)}")
